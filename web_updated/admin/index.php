05f6f2f3f1ff16e9dc79d2aba88914622ff5c016
Tom <vokiet2224@gmail.com>
2026-09-17 20:28:37 +0700

    Delete web_updated directory

--- web_updated/admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . "/../admin_lib/admin_verifier.modules.php";
-header("Location: dashboard.php");
-exit();
--- web_updated/lib/pageModules.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . "/../database/connect.php";
-
-function getPageTitle(){
-
-}
